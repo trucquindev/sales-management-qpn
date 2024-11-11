2a457b69820f458a850bfc4683f918958cadf622
Xuan Phu <xuanphu1024@gmail.com>
2024-11-11 10:20:29 +0700

    feat(master): refactor code with generouted

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import { Routes } from '@generouted/react-router';
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(<Routes />);
