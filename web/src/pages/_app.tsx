1575d74305051ba46fe004d68d5f95192772aac2
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-11-11 10:55:21 +0700

    update layout

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from 'react-router-dom';
+import Header from '../components/header/Header';
 // const queryClient = new QueryClient({
 //   defaultOptions: {
 //     queries: {
@@ -11,7 +12,8 @@ import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
   return (
-    <div className="min-h-screen w-screen">
+    <div className=" m-w-2xl mx-auto">
+      <Header />
       <Outlet />
     </div>
   );
