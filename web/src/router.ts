5b8fdd8dc2cccbf8390e3caaa7968f53fe0461cc
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-11-12 21:18:59 +0700

    feat(shopping-card-popup): custom shoping card

--- web/src/router.ts
+++ web/src/router.ts
@@ -1,20 +1,21 @@
 // Generouted, changes to this file will be overridden
 /* eslint-disable */
 
-import { components, hooks, utils } from '@generouted/react-router/client';
+import { components, hooks, utils } from '@generouted/react-router/client'
 
-export type Path = `/` | `/ok/aaaa` | `/blog` | `/blog/:blogId` | `/shop`;
+export type Path =
+  | `/`
+  | `/about`
+  | `/blog`
+  | `/blog/:blogId`
+  | `/shop`
 
 export type Params = {
-  '/blog/:blogId': { blogId: string };
-};
+  '/blog/:blogId': { blogId: string }
+}
 
-export type ModalPath = never;
+export type ModalPath = never
 
-export const { Link, Navigate } = components<Path, Params>();
-export const { useModals, useNavigate, useParams } = hooks<
-  Path,
-  Params,
-  ModalPath
->();
-export const { redirect } = utils<Path, Params>();
+export const { Link, Navigate } = components<Path, Params>()
+export const { useModals, useNavigate, useParams } = hooks<Path, Params, ModalPath>()
+export const { redirect } = utils<Path, Params>()
